82da1271e0647cbed26b56c392e14faa551f5e0e
Nimroh <91165013+Nimroh@users.noreply.github.com>
2021-09-24 06:07:02 -0500

    webservice
    Ya despliega un json con la tabla productos... y lo consume Angular

--- version1.2/frontend_angular/src/app/Modelo/Producto.ts
+++
@@ -1,9 +0,0 @@
-/*
-Se declaran las variables que arroja el webservice
-String, number
-*/
-
-export class Producto{
-    id_producto: number| undefined;
-    nombre_producto: String | undefined;
-}
